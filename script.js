d477de3d6a09445b7c2d8b158908f260920a6563
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-18 22:35:08 +0000

    unsure how to fix getCountryByName() method

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const countries = document.getElementById("countriesList");
+const form = document.getElementById("countriesForm")
 
 function name(countryData){
     let countryName = countryData[0].name;
@@ -15,39 +16,51 @@ function countryInfo(countryData){
 const getAllCountries=
 fetch("https://restcountries.com/v2/all")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data))
+    .then(() => heading.innerHTML = "Countries")
+    console.log(getAllCountries)
 
-function map(countryData){
-    let countries = countryData;
-    for(let country of countries){
-        let countryInfo = grabData(country);
-        const newHeader = document.createElement("h3");
-        newHeader.append(countryInfo);
-        
-    };
 
-}
+// Using a MAP?
+// function map(countryData){
+//     let countries = countryData;
+//     for(let country of countries){
+//         let countryInfo = grabData(country);
+//         const newHeader = document.createElement("h3");
+//         newHeader.append(countryInfo);
+//         countries.append(newHeader);
+//     };
+
+// }
 
 const getCountryByName = async (countryName)=>{
     const response = await fetch ('https://restcountries.com/v3.1/name/${countryName}');
     const jsonData = await response.json();
+    // const countryInfo = await response.json();
+   
     console.log(jsonData);
 
-    const countryInfo = await response.json();
-    jsonData.forEach(country => {
-    const content = document.createElement("h2");
-    content.textContent = "The name of country is " + country.name.nativeName.tur.official +
-    " Population is " + country.population;
-    console.log(content);
+
+    const countriesSection = document.querySelector
+
+    // jsonData.forEach(country => {
+    // const content = document.createElement("h2");
+    // content.textContent = jsonData[0].name.common;
+    // // content.textContent = "The name of country is " + country.name.nativeName.tur.official +
+    // // " Population is " + country.population;
+    // console.log(content);
 
     countryInfo.appendChild(content);
 
     console.log(countryInfo)
     document.querySelector("body").appendChild(content);
-    })
-}
+    countriesSection.appendChild(content)
+
+    }
+
 
 getCountryByName("russia")
+
 
 const button = document.querySelector("button");
 button.addEventListener("click", getCountryByName)
